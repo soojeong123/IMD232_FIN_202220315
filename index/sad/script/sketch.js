a2827cbefdcc6ee6d677fa3309cfd0f815f262b8
soojeong123 <126849562+soojeong123@users.noreply.github.com>
2023-12-20 21:11:53 +0900

    각주

--- index/sad/script/sketch.js
+++ index/sad/script/sketch.js
@@ -1,3 +1,6 @@
+//참고 Web Bae의 'Picture to Particles'
+//https://webflow.com/made-in-webflow/website/picture-to-particles?utm_medium=affiliate&ps_partner_key=a2VlZ2FubGVhcnk4Njcy&ps_xid=h8pb9E7TZK6YMO&gsxid=h8pb9E7TZK6YMO&gspk=a2VlZ2FubGVhcnk4Njcy
+
 //크기
 const particleSize = 5;
 //간격
@@ -30,15 +33,19 @@ function draw() {
 }
 
 function spawnParticles() {
+  //파티클을 그리드 형태로 배치
   for (let i = 0; i < width; i += RESOLUTION) {
     for (let j = 0; j < height; j += RESOLUTION) {
       let x = (i / width) * img.width;
       let y = (j / height) * img.height;
 
+      //color에 각 좌표의 이미지 색상 데이터를 저장
       const color = img.get(x, y);
       // console.log({ color });
 
       particles.push(
+        //화면에서 벗어나지 않게 하기 위해 x, y에 파티클 사이즈/2 씩을 더해준다.
+        //파티클에 같은 위치에 있는 이미지의 색상이 들어옴
         new Particle(i + particleSize / 2, j + particleSize / 2, color)
       );
     }
